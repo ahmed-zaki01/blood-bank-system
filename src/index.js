400a763c93dd494b4beb811b2ad3b2839ecfacec
Ahmed Zaki <ahmed.zaki0263@gmail.com>
2019-10-14 02:10:42 +0200

    upload initial files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./css/all.min.css";
+import "./css/bootstrap.min.css";
+import "./css/style.css";
+
+
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/popper.js/dist/popper.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
